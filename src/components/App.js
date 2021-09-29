fc5268b0aee8c5e1b86614a420f4d3eececbf77d
VictorCR18 <Cavalcantevictor2003@gmail.com>
2021-09-29 20:40:16 -0300

    adicionando rotas

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Post from './Post';
+import Routes from './routes';
 
 export default class app extends React.Component {
     render() {
         return (
         <div>
+            <Routes/>
             <h1>Hello World</h1> 
             <Post title="Aprendendo React JS"/>
-            <Post title="Socorro"/>
+            <Post title="Testando"/>
             <Post title="Aaaaaaaaaaaaaaaaaa"/>
         </div>
         );
